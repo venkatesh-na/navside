59fc1bf629e659c08b52d490cfa900ce9d27bd42
venkatesh-na <naikvenkatesh723@gmail.com>
2021-09-30 18:19:40 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
+import Header from "./header";
+import { FaBars } from "react-icons/fa"
+import { useGlobalContext } from "./context"
 
 function App() {
+  const { handleBar } = useGlobalContext()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <FaBars onClick = {handleBar} className = "bar"/>
+      <Header/>
+    </React.Fragment>
   );
 }
 
